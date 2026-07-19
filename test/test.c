bf903cd19aeb5c4e80a03dff410e7605eec8d895
Arata <49086220+arata-nvm@users.noreply.github.com>
2026-07-19 18:03:00 +0900

    Completion of step7

--- test/test.c
+++ test/test.c
@@ -82,8 +82,8 @@ app_main(void)
     dst = src;
     debugf("press Ctrl+C to terminate");
     while (!terminate) {
-        if (ip_output(1, test_data + offset, sizeof(test_data) - offset, src, dst) == -1) {
-            errorf("net_device_output() failure");
+        if (ip_output(IP_PROTOCOL_ICMP, test_data + offset, sizeof(test_data) - offset, src, dst) == -1) {
+            errorf("ip_output() failure");
             break;
         }
         sleep(1);
